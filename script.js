6fe9ca021db907dcf0f967a43ee5584849f392b2
AntwonTheDamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-03-05 16:51:24 -0500

    Implement form validation, add accessability attributes

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
+const inputs = document.querySelectorAll('input');
 const pswd = document.querySelector('#pswd');
-const pswdCheck = document.querySelector('#checkpswd');
+const checkPswd = document.querySelector('#checkpswd');
+const pswdError = document.querySelector('#pswderror');
+let pswdBool = false;
+
+pswd.addEventListener('change', () => {
+    if (pswd.value !== '') {
+        pswdBool = true;
+    } else {
+        pswdBool = false;
+    }
+});
+
+checkPswd.addEventListener('change', () => {
+    if (checkPswd.value === '') {
+        pswdError.textContent = '';
+    } else if (pswdBool === true && pswd.value !== checkPswd.value) {
+        pswdError.textContent = '*error passwords do not match';
+    } else {
+        pswdError.textContent = '';
+    }
+});
+
+inputs.forEach(input => {
+    input.addEventListener('focusout', ()  => {
+        if (input.value === '') {
+            input.classList = '';
+        } else {
+            input.classList = 'triggered';
+        }
+    });
+});
